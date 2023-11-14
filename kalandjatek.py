470686ae2d8d2bcfe619be35adca52f01c3ae22d
Papp Péter <pepa@ADY-NAGYATAD.SULINET.HU>
2023-11-14 11:22:47 +0100

    játéktörténet

--- kalandjatek.py
+++ kalandjatek.py
@@ -48,6 +48,84 @@ tortenet=[
     ]
 ]
 
+tortenet=[
+    [
+        1,  #történetszál sorszáma
+        "Felébredtem.Hol vagyok? Ki kell innen találnom!",    #történetszál szövege
+        ["lefelé", "jobbra"],#választható cselekvések
+        [4,2]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        12,  #történetszál sorszáma
+        "Itt ébredtem! Most másfelé próbálkozzunk!",    #történetszál szövege
+        ["lefelé", "jobbra"],#választható cselekvések
+        [4,2]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        2,  #történetszál sorszáma
+        "Itt nincs kijárat. Keressünk tovább!",    #történetszál szövege
+        ["lefelé", "jobbra","balra"],#választható cselekvések
+        [5,3,12]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        3,  #történetszál sorszáma
+        "Itt sincs kijárat. Keressünk tovább!",    #történetszál szövege
+        ["lefelé", "balra"],#választható cselekvések
+        [6,2]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        4,  #történetszál sorszáma
+        "Ez egy üres szoba. Keressünk tovább!",    #történetszál szövege
+        ["felfelé", "jobbra"],#választható cselekvések
+        [12,5]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        5,  #történetszál sorszáma
+        "Ez valami központi folyosó lehet. De nincs kijárat. Keressünk tovább!",    #történetszál szövege
+        ["felfelé", "lefelé","jobbra","balra"],#választható cselekvések
+        [2,8,6,4]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        6,  #történetszál sorszáma
+        "Friss levegőt érzek! Keressünk tovább!",    #történetszál szövege
+        ["felfelé", "lefelé","balra"],#választható cselekvések
+        [3,9,5]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        7,  #történetszál sorszáma
+        "Ez zsákutca. Menjünk vissza!",    #történetszál szövege
+        ["vissza"],#választható cselekvések
+        [8]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        8,  #történetszál sorszáma
+        "Már unom... Keressünk tovább!",    #történetszál szövege
+        ["felfelé","balra"],#választható cselekvések
+        [5,7]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        9,  #történetszál sorszáma
+        "Már látom a fényt!",    #történetszál szövege
+        ["felfelé","lefelé"],#választható cselekvések
+        [6,10]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        10,  #történetszál sorszáma
+        "Végre kijutottam! Merre tovább?",    #történetszál szövege
+        ["vissza","el innen"],#választható cselekvések
+        [9,11]  #cselekvés sorszáma, hova ugorjon
+    ],
+    [
+        11,  #történetszál sorszáma
+        "Isten véled rossz emlék!",    #történetszál szövege
+        [],#választható cselekvések
+        []  #cselekvés sorszáma, hova ugorjon
+    ],
+
+]
+#        ["előre", "lefelé", "jobbra","balra"],#választható cselekvések
+
+
 szalId=1
 while True:
     tempLista=[]
@@ -56,14 +134,19 @@ while True:
 
     keresettIndex=tempLista.index(szalId)
 
+    print("\n"+"-"*50)
     print(tortenet[keresettIndex][1])
+
+    if len(tortenet[keresettIndex][2])==0:
+        break
     valasztott=menu(tortenet[keresettIndex][2])
     
-    szalId=tortenet[keresettIndex][3][valasztott]
     if szalId==999:
         break
-    
 
+    szalId=tortenet[keresettIndex][3][valasztott]
+    
+print("VÉGE")
 
 
 
